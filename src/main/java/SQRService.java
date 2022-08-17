15f36f649b8a7fbc830c0631c82782ca2e56d6b2
Абдуллин Рушан <rushan.lucky@yandex.ru>
2022-08-17 22:42:26 +0500

    Changes

--- src/main/java/SQRService.java
+++ src/main/java/SQRService.java
@@ -1,13 +1,13 @@
 public class SQRService {
-    public int calculateSquared() {
+    public int calculateSquared(int rangeMin, int rangeMax) {
         SQRService service = new SQRService();
         int numberMin = 10;
         int numberMax = 99;
         int counter = 0;
         for (int i = numberMin; i < numberMax; i++)
-            if (i * i >= 300 && i * i <= 900)
+            if (i * i >= rangeMin && i * i <= rangeMax)
                 counter++;
             System.out.println("Количество квадратов чисел в диапазоне равно " + counter++);
-        return counter++;
+        return counter;
     }
 }
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -7,7 +7,22 @@ public class SQRServiceTest {
     void shouldRangeSqrService() {
         SQRService service = new SQRService();
         int expected = 14;
-        int actual = service.calculateSquared();
+        int actual = service.calculateSquared(300,900);
+        assertEquals(expected, actual);
+    }
+    @Test
+    void rangeMinimal() {
+        SQRService service = new SQRService();
+        int expected = 1;
+        int actual = service.calculateSquared(99,100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void rangeMaximal() {
+        SQRService service = new SQRService();
+        int expected = 1;
+        int actual = service.calculateSquared(9801,9802);
         assertEquals(expected, actual);
     }
 }
